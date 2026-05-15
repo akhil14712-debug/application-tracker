795bca3133daccaf9a5835a629b21d97eaecf68c
akhil14712-debug <akhil14712@gmail.com>
2026-05-15 16:02:46 +0530

    Cors issue solved

--- src/main/java/com/applicationtracker/config/SecurityConfig.java
+++ src/main/java/com/applicationtracker/config/SecurityConfig.java
@@ -52,8 +52,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOriginPatterns(List.of("http://localhost:*",
-                "https://*.vercel.app"));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
